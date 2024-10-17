fd257d7360d078d5b0bbb4bb6817e3dae6665506
mohamedsaeed253 <muhammedsaeed253@gmail.com>
2024-10-17 18:22:08 +0300

    modifying card component

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,14 +1,7 @@
 import { Link } from "react-router-dom";
 import "./Card.css";
-/* import { useDispatch } from "react-redux";
-import { getType } from "../../redux/actions/details-actions"; */
-
 export default function Card(props) {
-/*   const dispatch = useDispatch(); */
   let { data, type, page } = props;
-/*   const sentInfo = () => {
-    dispatch(getType(data));
-  }; */
 
   let link = `https://image.tmdb.org/t/p/w500/${data.poster_path}`;
   return (
@@ -21,9 +14,6 @@ export default function Card(props) {
           to={`../${type || data.media_type}/${page || "trend"}/${
             data.id
           }/details`}
-/*           onClick={() => {
-            sentInfo();
-          }} */
         >
           {data.name || data.title}
         </Link>
